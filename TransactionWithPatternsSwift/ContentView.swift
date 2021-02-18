eec1fb6a026a102abe26d7b081d8404b3ddb0a53
carlos apablaza <c.apablaza.z@gmail.com>
2021-02-18 11:05:32 -0300

    Initial Commit

---
+++ TransactionWithPatternsSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TransactionWithPatternsSwift
+//
+//  Created by carlos alberto apablaza zamorano on 18-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TransactionWithPatternsSwift/TransactionWithPatternsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionWithPatternsSwiftApp.swift
+//  TransactionWithPatternsSwift
+//
+//  Created by carlos alberto apablaza zamorano on 18-02-21.
+//
+
+import SwiftUI
+
+@main
+struct TransactionWithPatternsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
